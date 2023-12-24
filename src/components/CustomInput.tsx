f458492fa554d5cc4b3ad09721e994c95533eaa3
Nikolaev <nikolaev-dmitrii2001@mail.ru>
2023-12-24 13:58:56 +0500

    review

--- src/components/CustomInput.tsx
+++ src/components/CustomInput.tsx
@@ -13,10 +13,11 @@ const CustomInput = ({ data, options, handleChange }: ICustomInputProps) => {
   const [isOpen, setIsOpen] = useState(false);
   const myInput = useRef<HTMLInputElement | null>(null);
   const myButton = useRef<HTMLDivElement | null>(null);
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState(data[options.field]);
 
-  const onEnter = (event: React.KeyboardEvent) => {
-    if (event.key === 'Enter' && value) {
+  const onKeyDown = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      console.log('enter');
       handleChange(data.id, value);
       setIsOpen(false);
     }
@@ -38,15 +39,10 @@ const CustomInput = ({ data, options, handleChange }: ICustomInputProps) => {
     }
   };
 
-  useEffect(() => {
-    setValue(data[options.field]);
-    console.log('parent');
-  }, [data]);
+ 
 
   useEffect(() => {
-    if (isOpen) {
-      myInput.current?.focus();
-    }
+      isOpen && myInput.current?.focus();
   }, [isOpen]);
 
   useEffect(() => {
@@ -73,7 +69,7 @@ const CustomInput = ({ data, options, handleChange }: ICustomInputProps) => {
   return (
     <div onClick={(event) => event.stopPropagation()}>
       <InputText
-        onKeyDown={onEnter}
+        onKeyDown={onKeyDown}
         ref={myInput}
         type="text"
         value={value}
